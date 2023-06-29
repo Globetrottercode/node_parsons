a75db0936497005aa028889d240be63d1441afe8
Globetrottercode <dassouvik180@gmail.com>
2023-06-29 17:38:13 +0530

    MongoDB Atlas DB connection established

--- myapp.js
+++ myapp.js
@@ -1,9 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT;
+
+mongoose
+  .connect(
+    `mongodb+srv://globetrotter:${process.env.MONGO_URL}@cluster0.b3ghyne.mongodb.net/ParsonsDB`
+  )
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -13,7 +25,6 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  console.log("request recieved", req.body.id);
   res.json({ message: `This is the text you sent : ${req.body.id}` });
 });
 
